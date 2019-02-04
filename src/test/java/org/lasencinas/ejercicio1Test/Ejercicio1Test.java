ae1a0b8b8611475631af7a55913f84b9843ab9b9
alexnro <alexjandron99@gmail.com>
2019-02-04 17:13:40 +0100

    Test cases passed

--- src/test/java/org/lasencinas/ejercicio1Test/Ejercicio1Test.java
+++ src/test/java/org/lasencinas/ejercicio1Test/Ejercicio1Test.java
@@ -1,14 +1,18 @@
 package org.lasencinas.ejercicio1Test;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
+import org.lasencinas.ejercicio1.Ejercicio1;
 
 
 public class Ejercicio1Test {
 
     @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue( true );
+    public void menorVentaTest() {
+
+        Ejercicio1 menor = new Ejercicio1();
+
+        assertEquals(320.35, menor.menorVenta(), 320);
     }
 }
