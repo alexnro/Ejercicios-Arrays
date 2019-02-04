0afc88265ffa8b0e02886a5f3721cf411c9e383d
alexnro <alexjandron99@gmail.com>
2019-02-04 17:25:23 +0100

    Test cases passed

--- src/test/java/org/lasencinas/ejercicio1Test/Ejercicio1Test.java
+++ src/test/java/org/lasencinas/ejercicio1Test/Ejercicio1Test.java
@@ -2,17 +2,30 @@ package org.lasencinas.ejercicio1Test;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.lasencinas.ejercicio1.Ejercicio1;
 
 
 public class Ejercicio1Test {
 
+    private Ejercicio1 ejercicio = null;
+
+    @Before
+    public void init() {
+        ejercicio = new Ejercicio1();
+    }
+
     @Test
     public void menorVentaTest() {
 
-        Ejercicio1 menor = new Ejercicio1();
+        assertEquals(320.35, ejercicio.menorVenta(), 320);
+    }
 
-        assertEquals(320.35, menor.menorVenta(), 320);
+    @Test
+    public void mayorVentaTest() {
+
+            assertEquals(8000, ejercicio.mayorVenta(), 8000);
+        ;
     }
 }
