7dfddcfa7c551ad37438c3ef2299841caa80e852
alexnro <alexjandron99@gmail.com>
2019-02-11 22:04:32 +0100

    Logic for give numbers to list moved to ListaFinal class

--- src/main/java/org/lasencinas/ejercicio2/Ejercicio2.java
+++ src/main/java/org/lasencinas/ejercicio2/Ejercicio2.java
@@ -1,47 +1,44 @@
 package org.lasencinas.ejercicio2;
 
-import org.lasencinas.ejercicio2.ListaFinal
-import java.util.Random;
+import org.lasencinas.ejercicio2.ListaFinal;
 
 public class Ejercicio2 {
 
     public static void main(String[] args) {
-
+        
         ListaFinal resultado = new ListaFinal();
 
         // damos valores a lista
-        for (int i = 0; i < lista.length; i++) {
-            lista[i] = aleatorio.nextInt(26) + 25;
-        }
+        resultado.setListaAleatoria();
 
-        for (int i = 0; i < lista.length; i++) {
-            if (lista[i] % 2 == 0) {
-                auxiliar[0]++;
+        for (double i = 0; i < resultado.getLista().length; i++) {
+            if (resultado.getLista()[i] % 2 == 0) {
+                resultado.getAuxiliar()[0]++;
             } else {
-                auxiliar[1]++;
+                resultado.getAuxiliar()[1]++;
             }
             if (i == 0) {
-                min = lista[i];
-            } else if (lista[i] < min) {
-                min = lista[i];
+                resultado.min = resultado.getLista()[i];
+            } else if (resultado.getLista()[i] < resultado.getMin()) {
+                resultado.min = resultado.getLista()[i];
             }
         }
-        auxiliar[2] = sumaImpares / auxiliar[1];
-        for (int i = 1; i <= min; i++) {
-            auxiliar[3] += i;
+        resultado.getAuxiliar()[2] = resultado.getSumaImpares() / resultado.getAuxiliar()[1];
+        for (int i = 1; i <= resultado.getMin(); i++) {
+            resultado.getAuxiliar()[3] += i;
         }
 
-        for (int i = 0; i < lista.length; i++) {
-            System.out.println("Posicion " + i + " de la lista =" + lista[i]);
+        for (int i = 0; i < resultado.getLista().length; i++) {
+            System.out.println("Posicion " + i + " de la lista =" + resultado.getLista()[i]);
         }
         System.out.println();
 
-        System.out.println("Numero de elementos pares: " + auxiliar[0]);
-        System.out.println("Numero de elementos impares: " + auxiliar[1]);
-        System.out.println("Media impares: " + auxiliar[2]);
+        System.out.println("Numero de elementos pares: " + resultado.getAuxiliar()[0]);
+        System.out.println("Numero de elementos impares: " + resultado.getAuxiliar()[1]);
+        System.out.println("Media impares: " + resultado.getAuxiliar()[2]);
         System.out
                 .println("Suma de lso enteros comprendidos entre 1 y el minimo de la lista: "
-                        + auxiliar[3]);
+                        + resultado.getAuxiliar()[3]);
     }
 
 }
