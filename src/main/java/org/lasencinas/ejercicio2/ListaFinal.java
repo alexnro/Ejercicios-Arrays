409e2d1adb08dc07f02a38b045429c8866824b32
alexnro <alexjandron99@gmail.com>
2019-02-11 22:04:59 +0100

    Added method setListaAleatoria() with part of the logic

--- src/main/java/org/lasencinas/ejercicio2/ListaFinal.java
+++ src/main/java/org/lasencinas/ejercicio2/ListaFinal.java
@@ -4,11 +4,36 @@ import java.util.Random;
 
 public class ListaFinal {
 
-    int[] lista = new int[25];
+    double[] lista = new double[25];
     Random aleatorio = new Random();
     double[] auxiliar = new double[4];
     int sumaImpares = 0;
     int min = 0;
 
+    public double[] getLista() {
+        return lista;
+    }
 
+    public Random getAleatorio() {
+        return aleatorio;
+    }
+
+    public double[] getAuxiliar() {
+        return auxiliar;
+    }
+
+    public int getSumaImpares() {
+        return sumaImpares;
+    }
+
+    public int getMin() {
+        return min;
+    }
+
+    public double[] setListaAleatoria() {
+        for (int i = 0; i < this.getLista().length; i++) {
+            this.getLista()[i] = this.getAleatorio().nextInt(26) + 25;
+        }
+        return this.getLista();
+    }
 }
