c5d2214d6705d198056f97d0e3ecbdf8605590ac
alexnro <alexjandron99@gmail.com>
2019-02-11 22:05:37 +0100

    Create new test case but it needs to be fixed to work with every number of the list and not with the entire list

--- src/test/java/org/lasencinas/ejercicio2Test/Ejercicio2Test.java
+++ src/test/java/org/lasencinas/ejercicio2Test/Ejercicio2Test.java
@@ -1,8 +1,18 @@
 package org.lasencinas.ejercicio2Test;
 
 import org.junit.Test;
+import org.lasencinas.ejercicio2.Ejercicio2;
+import org.lasencinas.ejercicio2.ListaFinal;
+
+import static org.junit.Assert.assertEquals;
 
 public class Ejercicio2Test {
 
+    @Test
+    public void generarListaTest() {
+        ListaFinal ejercicio = new ListaFinal();
+
+        assertEquals(37.5, ejercicio.setListaAleatoria(), 12.5);
+    }
 
 }
